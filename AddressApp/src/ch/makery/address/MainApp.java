911d5da540171d933c338819dbe159013536d7c8
Andrew Johnson <abjohnson395@gmail.com>
2018-03-13 17:33:03 -0400

    Completed part 2. Created controller for the person overview pane

--- AddressApp/src/ch/makery/address/MainApp.java
+++ AddressApp/src/ch/makery/address/MainApp.java
@@ -2,18 +2,43 @@ package ch.makery.address;
 
 import java.io.IOException;
 
+import ch.makery.address.controller.PersonOverviewController;
+import ch.makery.address.model.Person;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+/**
+ * @author abjohnson395@gmail.com
+ *
+ */
 public class MainApp extends Application {
 
 	private Stage primaryStage;
 	private BorderPane rootLayout;
 	
+	private ObservableList<Person> personData = FXCollections.observableArrayList(); 
+	
+	/**
+     * Default Constructor
+     */
+    public MainApp() {
+        // Add some sample data
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -57,11 +82,19 @@ public class MainApp extends Application {
 			
 			// Setting person overview into the center of the root layout
 			rootLayout.setCenter(personOverview);
+			
+	        // Give the controller access to the main app.
+	        PersonOverviewController controller = loader.getController();
+	        controller.setMainApp(this);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	public ObservableList<Person> getPersonData() {
+        return personData;
+    }
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
