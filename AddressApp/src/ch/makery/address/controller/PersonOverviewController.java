eb652400a59d37a7caf02e36917dda6f4d3bbfa7
Andrew Johnson <abjohnson395@gmail.com>
2018-04-22 20:02:17 -0400

    Added new utility class and added change listener to the person table

--- AddressApp/src/ch/makery/address/controller/PersonOverviewController.java
+++ AddressApp/src/ch/makery/address/controller/PersonOverviewController.java
@@ -1,19 +1,13 @@
-/**
- * 
- */
 package ch.makery.address.controller;
 
 import ch.makery.address.MainApp;
 import ch.makery.address.model.Person;
+import ch.makery.address.util.DateUtil;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
-/**
- * @author abjohnson395@gmail.com
- *
- */
 public class PersonOverviewController {
     @FXML
     private TableView<Person> personTable;
@@ -39,7 +33,8 @@ public class PersonOverviewController {
     private MainApp mainApp;
 
     /**
-     * Default Constructor
+     * The constructor.
+     * The constructor is called before the initialize() method.
      */
     public PersonOverviewController() {
     }
@@ -50,9 +45,18 @@ public class PersonOverviewController {
      */
     @FXML
     private void initialize() {
-        // Initialize the person table with the two columns.
-        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
-        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+    	// Initialize the person table with the two columns.
+        firstNameColumn.setCellValueFactory(
+        		cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(
+        		cellData -> cellData.getValue().lastNameProperty());
+        
+        // Clear person details.
+        showPersonDetails(null);
+
+        // Listen for selection changes and show the person details when changed.
+		personTable.getSelectionModel().selectedItemProperty().addListener(
+				(observable, oldValue, newValue) -> showPersonDetails(newValue));
     }
 
     /**
@@ -65,5 +69,31 @@ public class PersonOverviewController {
 
         // Add observable list data to the table
         personTable.setItems(mainApp.getPersonData());
+    }
+    
+    /**
+     * Fills all text fields to show details about the person.
+     * If the specified person is null, all text fields are cleared.
+     * 
+     * @param person the person or null
+     */
+    private void showPersonDetails(Person person) {
+    	if (person != null) {
+    		// Fill the labels with info from the person object.
+    		firstNameLabel.setText(person.getFirstName());
+    		lastNameLabel.setText(person.getLastName());
+    		streetLabel.setText(person.getStreet());
+    		postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+    		cityLabel.setText(person.getCity());
+    		birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+    	} else {
+    		// Person is null, remove all the text.
+    		firstNameLabel.setText("");
+    		lastNameLabel.setText("");
+    		streetLabel.setText("");
+    		postalCodeLabel.setText("");
+    		cityLabel.setText("");
+    		birthdayLabel.setText("");
+    	}
     }
 }
